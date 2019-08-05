daa4359b51ae84f2ba619167d24d3708a0d9c6b4
Yang Liu <liuyang12@focusmedia.cn>
2019-08-05 15:27:08 +0800

    udpat

--- config.py
+++ config.py
@@ -5,25 +5,21 @@ import torch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # sets device for model and PyTorch tensors
 
 # Model parameters
-input_dim = 40  # dimension of feature
+input_dim = 80  # dimension of feature
 window_size = 25  # window size for FFT (ms)
+stride = 10  # window stride for FFT (ms)
 hidden_size = 512
 embedding_dim = 512
-stride = 10  # window stride for FFT
 cmvn = True  # apply CMVN on feature
 num_layers = 4
 
 # Training parameters
-batch_size = 32
-lr = 1e-3
-num_workers = 8  # for data-loading
 grad_clip = 5.  # clip gradients at an absolute value of
 print_freq = 100  # print training/validation stats  every __ batches
 checkpoint = None  # path to checkpoint, None if none
 
 # Data parameters
 IGNORE_ID = -1
-# pad_id = 0
 sos_id = 0
 eos_id = 1
 num_train = 120098

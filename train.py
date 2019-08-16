afc5dc2df491c0dd954aa5095d897220fa8531ff
Yang Liu <liuyang12@focusmedia.cn>
2019-08-16 14:56:33 +0800

    update

--- train.py
+++ train.py
@@ -49,6 +49,8 @@ def train_net(args):
         epochs_since_improvement = checkpoint['epochs_since_improvement']
         model = checkpoint['model']
         optimizer = checkpoint['optimizer']
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = args.lr
 
     logger = get_logger()
 

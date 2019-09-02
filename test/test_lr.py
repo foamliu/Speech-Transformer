e242e6a087947ab8c3e61e8f3959c9847b7888fd
Yang Liu <liuyang12@focusmedia.cn>
2019-09-02 09:48:21 +0800

    update

--- test/test_lr.py
+++ test/test_lr.py
@@ -1,19 +1,15 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 if __name__ == '__main__':
-    k = 0.0001
-    warmup_steps = 25000
-    max_lr = 1e-3
-    min_lr = 1e-5
+    k = 0.2
+    warmup_steps = 4000
+    d_model = 512
+    init_lr = d_model ** (-0.5)
 
     lr_list = []
-    for step_num in range(1, 100000):
-
-        lr = max_lr
-        if step_num > warmup_steps:
-            lr = max_lr * np.exp(-1.0 * k * (step_num - warmup_steps))
-            lr = max(lr, min_lr)
+    for step_num in range(1, 500000):
+        lr = k * init_lr * min(step_num ** (-0.5),
+                               step_num * (warmup_steps ** (-1.5)))
         lr_list.append(lr)
 
     print(lr_list[:100])
--- transformer/optimizer.py
+++ transformer/optimizer.py
@@ -4,14 +4,17 @@ import numpy as np
 class TransformerOptimizer(object):
     """A simple wrapper class for learning rate scheduling"""
 
-    def __init__(self, optimizer, max_lr=1e-3, min_lr=1e-5, warmup_steps=25000, k=0.0001):
+    def __init__(self, optimizer, max_lr=1e-3, min_lr=1e-5, warmup_steps=4000, k=0.2):
         self.optimizer = optimizer
-        self.max_lr = max_lr
-        self.min_lr = min_lr
+        self.k = k
+        self.warmup_steps = warmup_steps
+        d_model = 512
+        self.init_lr = d_model ** (-0.5)
+        self.lr = self.max_lr
         self.warmup_steps = warmup_steps
         self.k = k
         self.step_num = 0
-        self.lr = self.max_lr
+
 
     def zero_grad(self):
         self.optimizer.zero_grad()
@@ -22,8 +25,7 @@ class TransformerOptimizer(object):
 
     def _update_lr(self):
         self.step_num += 1
-        if self.step_num > self.warmup_steps:
-            self.lr = self.max_lr * np.exp(-1.0 * self.k * (self.step_num - self.warmup_steps))
-            self.lr = max(self.lr, self.min_lr)
-            for param_group in self.optimizer.param_groups:
-                param_group['lr'] = self.lr
+        self.lr = self.k * self.init_lr * min(self.step_num ** (-0.5),
+                                              self.step_num * (self.warmup_steps ** (-1.5)))
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = self.lr

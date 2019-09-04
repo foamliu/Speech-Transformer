a3db0396b23e829f0e15770191a79ecdd05acc31
Yang Liu <liuyang12@focusmedia.cn>
2019-09-04 17:35:36 +0800

    update

--- ngram_lm.py
+++ ngram_lm.py
@@ -10,17 +10,19 @@ with open(pickle_file, 'rb') as file:
     data = pickle.load(file)
 char_list = data['IVOCAB']
 samples = data['train']
-bigram_freq = collections.Counter()
+bigram_freq_counter = collections.Counter()
 
 for sample in tqdm(samples):
     text = sample['trn']
-    text = [char_list[idx] for idx in text]
+    # text = [char_list[idx] for idx in text]
     tokens = list(text)
     bigrm = nltk.bigrams(tokens)
     # print(*map(' '.join, bigrm), sep=', ')
 
     # get the frequency of each bigram in our corpus
-    bigram_freq.update(bigrm)
+    bigram_freq_counter.update(bigrm)
 
 # what are the ten most popular ngrams in this Spanish corpus?
-print(bigram_freq.most_common(10))
+print(bigram_freq_counter.most_common(10))
+
+bigram_freq = dict()

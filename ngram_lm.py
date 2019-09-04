53d00c498c4ab17a4d5b2fc1801ea316e1fe91b2
Yang Liu <liuyang12@focusmedia.cn>
2019-09-04 17:41:00 +0800

    update

--- ngram_lm.py
+++ ngram_lm.py
@@ -9,8 +9,9 @@ from config import pickle_file
 with open(pickle_file, 'rb') as file:
     data = pickle.load(file)
 char_list = data['IVOCAB']
+vocab_size = len(char_list)
 samples = data['train']
-bigram_freq_counter = collections.Counter()
+bigram_counter = collections.Counter()
 
 for sample in tqdm(samples):
     text = sample['trn']
@@ -20,9 +21,28 @@ for sample in tqdm(samples):
     # print(*map(' '.join, bigrm), sep=', ')
 
     # get the frequency of each bigram in our corpus
-    bigram_freq_counter.update(bigrm)
+    bigram_counter.update(bigrm)
 
 # what are the ten most popular ngrams in this Spanish corpus?
-print(bigram_freq_counter.most_common(10))
+print(bigram_counter.most_common(10))
 
 bigram_freq = dict()
+for key, value in bigram_counter.items():
+    bigram_freq[key] = value
+
+for i in range(vocab_size):
+    for j in range(vocab_size):
+        if (i, j) not in bigram_freq:
+            bigram_freq[(i, j)] = 1
+
+for i in range(vocab_size):
+    total = 0
+    for j in range(vocab_size):
+        total += bigram_freq[(i, j)]
+    for j in range(vocab_size):
+        bigram_freq[(i, j)] = bigram_freq[(i, j)] / total
+
+print(bigram_freq)
+
+with open('bigram_freq.pkl', 'wb') as file:
+    pickle.dump(bigram_freq, file)

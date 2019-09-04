0bae21b1251fc398eb2b0fa09e712e7958f4a12b
Yang Liu <liuyang12@focusmedia.cn>
2019-09-04 17:23:26 +0800

    update

--- test/test_lm.py
+++ test/test_lm.py
@@ -1,6 +1,26 @@
+import collections
+import pickle
+
 import nltk
-from nltk.tokenize import word_tokenize
-text = "to be or not to be"
-tokens = nltk.word_tokenize(text)
-bigrm = nltk.bigrams(tokens)
-print(*map(' '.join, bigrm), sep=', ')
+from tqdm import tqdm
+
+from config import pickle_file
+
+with open(pickle_file, 'rb') as file:
+    data = pickle.load(file)
+char_list = data['IVOCAB']
+samples = data['train']
+bigram_freq = collections.Counter()
+
+for sample in tqdm(samples):
+    text = sample['trn']
+    text = [char_list[idx] for idx in text]
+    tokens = list(text)
+    bigrm = nltk.bigrams(tokens)
+    # print(*map(' '.join, bigrm), sep=', ')
+
+    # get the frequency of each bigram in our corpus
+    bigram_freq.update(bigrm)
+
+# what are the ten most popular ngrams in this Spanish corpus?
+print(bigram_freq.most_common(10))
